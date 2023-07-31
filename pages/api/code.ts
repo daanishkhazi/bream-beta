c3dfa2cb1b99dbc2889a9626db1d321f3563c40c
Daanish <daanishkhazi@Daanishs-MacBook-Air.local>
2023-07-31 03:40:31 -0700

    admin done

--- pages/api/code.ts
+++ pages/api/code.ts
@@ -1,46 +1,64 @@
 // /api/code.ts
-import { NextApiRequest, NextApiResponse } from 'next'
-import { getSession } from '@auth0/nextjs-auth0'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from '@auth0/nextjs-auth0';
 import prisma from '@/lib/prisma';
 import crypto from 'crypto';
+import postmark from 'postmark';
 
 function generateUniqueCode() {
-    return crypto.randomBytes(8).toString('hex');
+  return crypto.randomBytes(8).toString('hex');
 }
 
-
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') {
-        return res.status(405).end();
-    }
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
+  const postmark = await import('postmark');
+  const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY!);
 
-    // Get the user session
-    const session = await getSession(req, res);
-    if (!session) {
-        // If no session exists, respond with a 401
-        return res.status(401).end();
-    }
-    // Get user info
-    const user = await prisma.user.findUnique({
-      where: { auth0Id: session.user.sub },
-    })
+  // Get the user session
+  const session = await getSession(req, res);
+  if (!session) {
+    // If no session exists, respond with a 401
+    return res.status(401).end();
+  }
+  
+  // Get user info
+  const user = await prisma.user.findUnique({
+    where: { auth0Id: session.user.sub },
+  });
 
-    if (!user) {
-      res.status(400).json({ error: 'User not found' })
-      return
-    }
+  if (!user) {
+    res.status(400).json({ error: 'User not found' });
+    return;
+  }
 
-    // Generate a unique code
-    const uniqueCode = generateUniqueCode();
+  // Generate a unique code
+  const uniqueCode = generateUniqueCode();
 
-    // Create new Code
-    const newCode = await prisma.code.create({
-      data: {
-        email: req.body.email,
-        code: uniqueCode,
-        tenantId: user.tenantId,
-      },
-    })
+  // Create new Code
+  const newCode = await prisma.code.create({
+    data: {
+      email: req.body.email,
+      code: uniqueCode,
+      tenantId: user.tenantId,
+    },
+  });
 
-    res.status(200).json(newCode)
+  // Send email
+  await client.sendEmailWithTemplate({
+    From: 'daanish@trybream.com',
+    To: req.body.email,
+    TemplateAlias: "user-invitation",
+    TemplateModel: {
+      code: uniqueCode,
+      product_name: 'Bream',
+      product_url: 'https://trybream.com',
+      company_name: 'Bream Labs, Inc.',
+      company_address: 'Sammamish, WA 98075',
+      support_email: 'daanish@trybream.com',
+    },
+  });
+
+  res.status(200).json(newCode);
 }
