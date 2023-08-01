c5784991c019647ce48ee972a151d5a6bedf63b8
Daanish <daanishkhazi@Daanishs-MacBook-Air.local>
2023-08-01 00:00:09 -0700

    gitignore

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="flex items-center justify-between p-6 border-t-4  border-black bg-[#FCFDF7]">
       <div className="font-medium">© 2023 Bream Labs, Inc.</div>
       <div className="flex items-center space-x-4">
-        <Link href="/help">Help</Link>
+        {/* <Link href="/help">Help</Link> */}
         <Link href="/privacy">Privacy</Link>
         <Link href="/terms">Terms</Link>
       </div>
